39dee8c2cba498960b0410dc40fe12d84bf29a26
Francesco Vollero <francesco.vollero@gmail.com>
2021-05-06 10:13:42 +0200

    [Spec][ProtectedId] Improved the spec

--- spec/machineid_protected_id_spec.cr
+++ spec/machineid_protected_id_spec.cr
@@ -2,17 +2,8 @@ require "./spec_helper"
 
 describe Machineid::ProtectedId do
   it "generate an HMAC-SHA256 hexdigest" do
-    Machineid::ProtectedId.protect("123stella", "123stella").should be_a(String)
+    # echo -n "123stella" | openssl dgst -sha256 -hmac "123stella"
+    # (stdin)= 791596ad9a4cbf86199315a86e183da34d7b970b06c507f04262a44f195b562f
+    Machineid::ProtectedId.protect("123stella", "123stella").should eq("791596ad9a4cbf86199315a86e183da34d7b970b06c507f04262a44f195b562f")
   end
 end
-
-#  # TODO: Write tests
-#  describe "Frontend" do
-#    it "works" do
-#      false.should eq(false)
-#    end
-#    it "#id" do
-#      Machineid.id.should be_a(String)
-#    end
-#  end
-# end
