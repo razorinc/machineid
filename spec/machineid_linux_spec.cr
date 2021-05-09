06f69d65b11e7150c014e82a5a383577ac630026
Francesco Vollero <francesco.vollero@gmail.com>
2021-05-10 01:24:19 +0200

    [Spec] Linux spec fix

--- spec/machineid_linux_spec.cr
+++ spec/machineid_linux_spec.cr
@@ -1,8 +1,8 @@
 require "./spec_helper"
 
-describe Machineid do
+describe Machineid::Linux do
   # TODO: Write tests
-  describe "Frontend" do
+  describe "Linux" do
     it "#id" do
       Machineid.id.should be_a(String)
     end
